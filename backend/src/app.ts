d7d80e897962d9fb65780c1fc17924eba6d6eeb4
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-12-29 10:33:07 +0530

    server created

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { logger } from './middleware/logger'
 import { notFound } from './middleware/notFound'
+import { errorHandler } from './middleware/errorHandler'
 const app = express()
 app.use(express.json())
 app.use(logger)
@@ -8,4 +9,5 @@ app.get('/health',(req,res)=>{
     res.json({status:"ok"})
 })
 app.use(notFound)
+app.use(errorHandler)
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import app from "./app";
 import dotenv from 'dotenv'
 dotenv.config()
+if (!process.env.PORT) {
+  throw new Error("PORT is not defined in environment variables");
+}
+
 app.listen(process.env.PORT,()=>{
     console.log('server running');
     
