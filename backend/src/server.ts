dc91516a497585cc933b1d30a08381ef9f2307fa
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-12-29 14:58:48 +0530

    neon db connected successfully

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,21 @@
 import app from "./app";
-import dotenv from 'dotenv';
-dotenv.config()
-import { pool } from "./config/db";
+import dotenv from "dotenv";
+dotenv.config();
+import { appDataSouce } from "./data-source";
 if (!process.env.PORT) {
   throw new Error("PORT is not defined in environment variables");
 }
 
 (async () => {
   try {
-    await pool.query("SELECT 1");
+    await appDataSouce.initialize();
     console.log("Database connected");
   } catch (error) {
-    console.error("Database connection failed");
+    console.error("Database connection failed", error);
     process.exit(1);
   }
 })();
 
-
-app.listen(process.env.PORT,()=>{
-    console.log('server running');
-    
-})
+app.listen(process.env.PORT, () => {
+  console.log("server running");
+});
