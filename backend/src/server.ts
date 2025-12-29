608662a1db7771dd3e6d01698618db483059bac8
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-12-29 11:28:25 +0530

    database connected

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,21 @@
 import app from "./app";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config()
+import { pool } from "./config/db";
 if (!process.env.PORT) {
   throw new Error("PORT is not defined in environment variables");
 }
+
+(async () => {
+  try {
+    await pool.query("SELECT 1");
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Database connection failed");
+    process.exit(1);
+  }
+})();
+
 
 app.listen(process.env.PORT,()=>{
     console.log('server running');
