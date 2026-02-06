0af0fc0ca3dac0700aec49c4b348688eaf664b35
NiranjanaNS <niranjanansajeev6952@gmail.com>
2026-02-06 09:33:05 +0530

    chore: user validation is completed

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,10 +1,9 @@
-import { z } from 'zod';
+// import { z } from 'zod';
 
-const envSchema = z.object({
-  JWT_SECRET: z.string().min(32),
-  REFRESH_TOKEN_SECRET: z.string().min(32),
-  NODE_ENV: z.enum(['development', 'production']),
-});
+// const envSchema = z.object({
+//   JWT_SECRET: z.string().min(32),
+//   REFRESH_TOKEN_SECRET: z.string().min(32),
+//   NODE_ENV: z.enum(['development', 'production']),
+// });
 
-envSchema.parse(process.env);
-//udfggf
+// envSchema.parse(process.env);
